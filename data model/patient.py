da77a70f5a15bddbbd5b25eb8a02d51ec4014898
vtomi97 <47665690+vtomi97@users.noreply.github.com>
2021-03-17 22:39:31 +0100

    DataModel

---
+++ data model/patient.py
@@ -0,0 +1,9 @@
+class Patient:
+
+    def __init__(self, _id, _gen, _weight, _spacing, _images):
+        self.id = _id
+        self.gender = _gen
+        self.weight = _weight
+        self.spacing = _spacing
+        self.images = _images
+
