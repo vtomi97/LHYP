c32f79266b66c50dc46abd226bd7d47f8a64c45f
vtomi97 <47665690+vtomi97@users.noreply.github.com>
2021-03-18 16:15:39 +0100

    Delete data model directory

--- data model/patient.py
+++
@@ -1,9 +0,0 @@
-class Patient:
-
-    def __init__(self, _id, _gen, _weight, _spacing, _images):
-        self.id = _id
-        self.gender = _gen
-        self.weight = _weight
-        self.spacing = _spacing
-        self.images = _images
-
